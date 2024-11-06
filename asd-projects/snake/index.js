8cc95454f0b1415554bd18d89b0fce38f1a95b13
kiragarretson <142329202+kiragarretson@users.noreply.github.com>
2024-11-06 15:37:53 +0000

    pushing the finished work

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -11,7 +11,8 @@ var highScoreElement = $("#highScore");
 
 // TODO 4a: Create the snake, apple and score variables
 // Game Variables
-
+var snake = [];
+var apple = [];
 // Constant Variables
 var ROWS = 20;
 var COLUMNS = 20;
