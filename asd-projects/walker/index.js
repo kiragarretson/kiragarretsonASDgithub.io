51cab52af8c3d7820be08ce57da5c827a5e2b331
kiragarretson <142329202+kiragarretson@users.noreply.github.com>
2024-12-16 16:03:43 +0000

    finished and fixed walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,11 +10,16 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  var BOARD_WIDTH = $("#board").width();
+  var BOARD_HEIGHT = $("#board").height();
+  var WALKER_WIDTH = $("#walker").width();
+  var WALKER_HEIGHT = $("#walker").height();
   var KEY = {
     UP: 38,
     DOWN: 40,
     LEFT: 37,
     RIGHT: 39,
+
     SPACE: 32,
 
     W: 87,
@@ -31,11 +36,18 @@ function runProgram(){
   speedY: 0
  }
 
+ var walker2 = {
+  xPos: 0,
+  yPos: 0,
+  speedX: 0,
+  speedY: 0
+ }
+
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on("keydown", handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on("keyup", handleKeyUp);
-
+  $(board).click(colorChange)
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -46,7 +58,8 @@ function runProgram(){
   */
   function newFrame() {
     repositionGameItem();
-
+    redrawGameItem();
+    wallCollision();
   }
   
   /* 
@@ -70,25 +83,80 @@ function runProgram(){
       walker.speedX = 5;
     }
 
-    function handleKeyUp(event){
-      if(event.which === KEY.DOWN || event.which === KEY.UP){
-        walker.speedY = 0;
+// start of player 2
+      if (event.which === KEY.W) {
+        console.log("W pressed");
+        walker2.speedY = -5;
       }
-      if(event.which === KEY.LEFT || event.which === KEY.RIGHT){
-        walker.speedX = 0;
+      if (event.which === KEY.S) {
+        console.log("S pressed");
+        walker2.speedY = +5;
+      }
+      if (event.which === KEY.A) {
+        console.log("A pressed");
+        walker2.speedX = -5;
+      }
+      if (event.which === KEY.D) {
+        console.log("D pressed");
+        walker2.speedX = 5;
+      }
+      if (event.which === KEY.SPACE) {
+        console.log("space pressed");
+        colorChange();
       }
   }
+  function handleKeyUp(event){
+    if(event.which === KEY.DOWN || event.which === KEY.UP){
+      walker.speedY = 0;
+    }
+    if(event.which === KEY.LEFT || event.which === KEY.RIGHT){
+      walker.speedX = 0;
+    }
 
+    if(event.which === KEY.S || event.which === KEY.W){
+      walker2.speedY = 0;
+    }
+    if(event.which === KEY.A || event.which === KEY.D){
+      walker2.speedX = 0;
+    }
+  }
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
   function repositionGameItem(){
     walker.xPos += walker.speedX;
     walker.yPos += walker.speedY;
-    $("#walker").css("top", walker.yPos);
-    $("#walker").css("bottom", walker.xPos);
+    walker2.xPos += walker2.speedX;
+    walker2.yPos += walker2.speedY;
   }
   
+  function redrawGameItem(){
+    $("#walker").css("top", walker.yPos);
+    $("#walker").css("left", walker.xPos);
+    $("#walker2").css("top", walker2.yPos);
+    $("#walker2").css("left", walker2.xPos);
+  }
+  function wallCollision(){
+    if(walker.xPos > BOARD_WIDTH - WALKER_WIDTH || walker.xPos < 0){
+      walker.xPos -= walker.speedX;
+    }
+    if(walker.yPos > BOARD_HEIGHT - WALKER_HEIGHT || walker.yPos < 0){
+      walker.yPos -= walker.speedY;
+    }
+    if(walker2.xPos > BOARD_WIDTH - WALKER_WIDTH || walker2.xPos < 0){
+      walker2.xPos -= walker2.speedX;
+    }
+    if(walker2.yPos > BOARD_HEIGHT - WALKER_HEIGHT || walker2.yPos < 0){
+      walker2.yPos -= walker2.speedY;
+    }
+  }
+
+  function colorChange(event){
+    var walkerColor = $("#walker").css("background-color");
+    var walker2Color = $("#walker2").css("background-color");
+      $("#walker").css("background-color", walker2Color);
+      $("#walker2").css("background-color", walkerColor)
+  }
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
