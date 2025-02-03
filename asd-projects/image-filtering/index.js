8d05018bc5fd62add62a1e9b9368553078953db2
kiragarretson <142329202+kiragarretson@users.noreply.github.com>
2025-02-03 16:08:16 +0000

    saving my progress

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,7 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
-
+applyFilter(reddify);
   
 
   // do not change the below line of code
@@ -33,7 +33,17 @@ function applyAndRender() {
 
 // TODO 1, 2 & 4: Create the applyFilter function here
 
-
+function applyFilter(filterFunction){
+  for(var i = 0; i < image.length; i++){
+    for(var j = 0; j < image[i].length; j++){
+      var rgbString = image[i][j];
+      var rgbNumbers = rgbStringToArray(rgbString);
+      filterFunction(rgbNumbers);
+      rgbString = rgbArrayToString(rgbNumbers);
+      image[i][j] = rgbString;
+    }
+  }
+}
 // TODO 7: Create the applyFilterNoBackground function
 
 
@@ -41,6 +51,9 @@ function applyAndRender() {
 
 
 // TODO 3: Create reddify function
+function reddify(array){
+ array[RED] = 200;
+}
 
 
 // TODO 6: Create more filter functions
